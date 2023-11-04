5042d666b8c93c10c60553d8e4d1a40d96b2768b
Roshan Singh <singh.roshan9718@gmail.com>
2023-11-04 14:34:59 +0530

    hashing

---
+++ Hashing/1.Hashing.js
@@ -0,0 +1,28 @@
+// hashing is mainly used to implement the dictonaries where we have key-value pairs.
+// it is also used to implement sets where we have only keys.
+
+// provide search, insert, delete all three operations in O(1) time on average
+// in hashing all the values are unique
+// it search exact key words.
+
+// not useful for: 
+//  1.finding closest value
+//  2.sorted data
+//  3.prefix searching
+
+// for 1 and 2, AVL Tree or Red Blacl Tree is used.
+// for 3, Trie DS is used
+
+
+
+
+// Application of Hashing, most used DS after Array..
+// 1. 
+
+
+
+
+// Direct Address Table
+
+// DAT is a data structure that has the capability of mapping records to their corresponding keys using arrays.
+// In DAT, records are placed using their key values directly as indexes. They facilitate fast searching, insertion and deletion operations.
---
+++ Sorting/3.insertionsort.js
@@ -0,0 +1,9 @@
+// -- O(n2) in worst case
+// -- In-place and stable
+// -- Used in practice for small arrays (TimSort and IntroSort)
+// -- O(n) in best case
+// -- O(n) in Best Case
+// -- best case happens when array is already sorted and worst case happens when array is reverse sorted.
+// -- time complexity in best case is θ(n) -- already sorted
+// worst case: reverse sorted θ(n2)
+// in general : O(n2)
